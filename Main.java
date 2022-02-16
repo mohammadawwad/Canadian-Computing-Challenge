8a13ad16bc881fe15520cca4e6ca35d5161389d7
mohammadawwad <mohd-awwad@hotmail.com>
2022-02-15 23:23:55 -0800

    setting up

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    
-  }
+      
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter Numbers");
+    String in = sc.nextLine();
+    String [] in1 = in.split("\\s+");
+    System.out.println("Numbers");
+    for( String n: in1)
+    {
+      System.out.println(n);
+    }
+  } 
 }
